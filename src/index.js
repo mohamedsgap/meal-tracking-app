d1cd6ed26e863590871e18564977c564872c7c71
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-11 10:03:36 +0200

    Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
-const store = createStore(reducer)
+  
+//const store = createStore(reducer)
 
 console.log(store)
 
