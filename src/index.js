fedd74f5c2a2a7935efdee8cf7c8b4a7bdb191f4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-12 15:37:50 +0200

    using the Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(
     reducer,
@@ -16,6 +17,6 @@ const store = createStore(
 
 console.log(store)
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store} ><App  /></Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
