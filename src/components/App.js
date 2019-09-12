4cc01852885b40c72bd6e2d3331252098a920188
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-12 16:16:48 +0200

    feat: mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
   
@@ -30,6 +30,15 @@ function mapStateToProps (calendar) {
   }
 }
 
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(App) 
