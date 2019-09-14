dff0b9976f457aa31d4d3fc5ad94ce225c21c9d1
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-09-14 18:19:53 +0200

    render calender grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addRecipe, removeFromCalendar } from '../actions'
+import { capitalize } from '../utils/helpers'
+import { FaCalendar } from "react-icons/fa"
 
 class App extends Component {
-  
   render() {
-    console.log(this.props)
+    const { calendar, remove } = this.props
+    const mealOrder = ['breakfast', 'lunch', 'dinner']
+
+
     return (
-      <div>
-         Hello, World!
+      <div className='container'>
+
+        <ul className='meal-types'>
+          {mealOrder.map((mealType) => (
+            <li key={mealType} className='subheader'>
+              {capitalize(mealType)}
+            </li>
+          ))}
+        </ul>
+
+        <div className='calendar'>
+          <div className='days'>
+            {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+          </div>
+          <div className='icon-grid'>
+            {calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                {mealOrder.map((meal) => (
+                  <li key={meal} className='meal'>
+                    {meals[meal]
+                      ? <div className='food-item'>
+                          <img src={meals[meal].image} alt={meals[meal].label}/>
+                          <button onClick={() => remove({meal, day})}>Clear</button>
+                        </div>
+                      : <button className='icon-btn'>
+                          <FaCalendar size={30}/>
+                        </button>}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+
       </div>
     )
   }
@@ -24,7 +60,8 @@ function mapStateToProps ({ food, calendar }) {
         meals[meal] = calendar[day][meal]
           ? food[calendar[day][meal]]
           : null
-          return meals
+
+        return meals
       }, {})
     })),
   }
@@ -37,8 +74,7 @@ function mapDispatchToProps (dispatch) {
   }
 }
 
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(App) 
+  mapDispatchToProps
+)(App)
