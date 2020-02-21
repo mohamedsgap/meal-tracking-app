05bfa8da459613a329008d25cbcc3b92a33e40e7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-21 20:37:56 +0200

    Updates

---
+++ precache-manifest.2021a301e206af02b9d9b22ff8ffe774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723a7a5448e8c5af419ec73efdb76c55",
+    "url": "/meal-tracking-app/index.html"
+  },
+  {
+    "revision": "52607f0fc8920268aa9f",
+    "url": "/meal-tracking-app/static/css/main.953dda45.chunk.css"
+  },
+  {
+    "revision": "66959d26153bb28be458",
+    "url": "/meal-tracking-app/static/js/2.7a92544f.chunk.js"
+  },
+  {
+    "revision": "52607f0fc8920268aa9f",
+    "url": "/meal-tracking-app/static/js/main.847b84f7.chunk.js"
+  },
+  {
+    "revision": "757dd48f982ad936b7c7",
+    "url": "/meal-tracking-app/static/js/runtime~main.cf29c52e.js"
+  }
+]);
